2aab7b65981a506e091c8ff9170a7bbdd0847c37
w44607797 <93862381+w44607797@users.noreply.github.com>
2022-05-25 12:23:38 +0800

    Add files via upload

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="hello">
+    <div class="left-bord">
+        真没空写这个啊啊啊啊啊
+    </div>
+    <div class="right-bord">
+    <h1>{{ msg }}</h1>
+
+    </div>
+
+
+
+
+  </div> 
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+body{
+ position: absolute;
+ display: flex;
+}
+.right-bord{
+    text-align: center;
+}
+.left-bord{
+
+  width: 20%;
+  height: 100%;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <HelloWorld msg="仓库管理系统"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
